638bacfe204a82630d4c99bb8bb4a5a9965d3b2e
MatheusTSantos <matheus.santos@pulsecrm.com.br>
2022-04-06 00:16:10 -0300

    adicionando informações no indexed db

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 // In the following line, you should include the prefixes of implementations you want to test.
 const idxDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
@@ -18,9 +18,11 @@ Object.defineProperty(window, 'IDBKeyRange', {
 	value: idxDBRange
 });
 const DBNAME = "MySneakers"
-const DBVERSION = 5
+const DBVERSION = 7
 // (Mozilla has never prefixed these objects, so we don't need window.mozIDB*)
 function App() {
+
+	let dbStore = useRef(null)
 
 	useEffect(() => {
 		const request = window.indexedDB.open(DBNAME, DBVERSION);
@@ -34,21 +36,33 @@ function App() {
 		};
 
 		request.onsuccess = event => {
-			const db = event.target;
-			console.log("success: ", db)
-
+			const db = event.target.result;
+			console.log("success: ", event.target.result)
+			dbStore.current = db
 		};
 
 		// only here you can change the structure of the store
 		request.onupgradeneeded = event => {
 			const db = event.target.result
 			const currentStores = Array.from(db.objectStoreNames)
-			
+
 			const customerExists = currentStores.find(store => store === 'customers')
 			if (!customerExists) {
 				const objectStore = db.createObjectStore("customers", { keyPath: "fiscal_code" });
 				objectStore.createIndex("name", "name", { unique: false });
-				objectStore.createIndex("email", "email", { unique: false });
+				objectStore.createIndex("email", "email", { unique: true });
+				console.log({ objectStore })
+			}
+
+			const paymentFormsExists = currentStores.find(store => store === 'paymentForms')
+			if (!paymentFormsExists) {
+				const objectStore = db.createObjectStore("paymentForms", { autoIncrement: true });
+				objectStore.createIndex("name", "name", { unique: true });
+				console.log({ objectStore })
+			} else {
+				db.deleteObjectStore("paymentForms");
+				const objectStore = db.createObjectStore("paymentForms", { autoIncrement: true });
+				objectStore.createIndex("name", "name", { unique: true });
 				console.log({ objectStore })
 			}
 
@@ -56,9 +70,43 @@ function App() {
 
 	}, [])
 
+	const [val, setVal] = useState('')
+	const handleChange = event => {
+		setVal(event.target.value)
+	}
+
+	const handleAdd = () => {
+		console.log(val)
+		if (!val) {
+			return
+		}
+		const objStore = dbStore.current
+			.transaction(['paymentForms'], 'readwrite')
+			.objectStore("paymentForms")
+		
+		console.log("vai adicionar algo nesse inferno", objStore.add)
+		const objectStoreRequest = objStore.add({ name: val })
+
+		objectStoreRequest.onsuccess = event => {
+			console.log("All done!", val);
+		};
+
+		objectStoreRequest.onerror = event => {
+			console.error("que maraviiiilha", event);
+		};
+	}
+
 	return (
 		<div className="App">
-
+			<input
+				onChange={handleChange}
+				value={val}
+			/>
+			<button
+				onClick={handleAdd}
+			>
+				add
+			</button>
 		</div>
 	);
 }
